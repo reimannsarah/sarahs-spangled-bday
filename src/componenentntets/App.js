5022c8fc03acc35998f00fed7d3b288bc48e70a8
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-18 14:55:27 -0700

    move file into components folder
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/componenentntets/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import PartyControl from './PartyControl';
+
+function App() {
+  return (
+    <div className="App">
+      <PartyControl />
+    </div>
+  );
+}
+
+export default App;
