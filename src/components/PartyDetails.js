12d8fef2022aacfca8384fccd42dc989853724ac
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-19 10:41:43 -0700

    create party details component
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/components/PartyDetails.js
@@ -0,0 +1,14 @@
+function PartyDetails() {
+  return (
+    <div>
+      <h1>Sarah's Spangled Birthday Smash</h1>
+      <h4>When: </h4>
+      <p>September 16 at 6pm</p>
+      <h4>Where: </h4>
+      <p>Sarah and Lucy's Castle</p>
+      <h4>What: </h4>
+      <p>Rage Room get your crank on and bring something to smash</p>
+    </div>
+  )
+}
+export default PartyDetails;
