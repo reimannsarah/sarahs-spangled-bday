10e07a545384daae3d576f578dffd65bd4cb6a3f
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-19 16:23:21 -0700

    remove label
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -3,17 +3,13 @@ import React from "react";
 function ReusableForm(props) {
   return(
     <form onSubmit={props.onPost}>
-      <label htmlFor="name">Name:</label>
-      <input type="text" name="name" id="name" required />
+      <input type="text" name="name" id="name" placeholder="NAME" required />
 
-      <label htmlFor="email">Email:</label>
-      <input type="email" name="email" id="email" required />
+      <input type="email" name="email" id="email" placeholder="EMAIL" required />
 
-      <label htmlFor="species">Species:</label>
-      <input type="text" name="species" id="species" required />
+      <input type="text" name="species" id="species" placeholder="SPECIES" required />
 
-      <label htmlFor="belief">Do You Believe?</label>
-      <textarea name="belief" id="belief" required ></textarea>
+      <textarea name="belief" id="belief" placeholder="DO YOU BELIEVE?" required ></textarea>
 
       <button type="submit">RSVP Me</button>
     </form>
