1e810a875a465e5bb4b6d2141f340318022a4d42
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-19 10:42:14 -0700

    create confirm rsvp component
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/components/RSVPConfirm.js
@@ -0,0 +1,22 @@
+function RSVPConfirm(props) {
+  const thisUser = props.userInfo
+  return (
+    
+    <div>
+      <h1>RSVP Confirmation</h1>
+      <p>Does your information look correct?</p>
+      <hr />
+      <p>Name: {thisUser.name}</p>
+      <p>Email: {thisUser.email}</p>
+      <p>Species: {thisUser.species}</p>
+      <p>Plus One? {thisUser.plusOne}</p>
+      <p>Do You Believe? {thisUser.doYouBelieve}</p>
+      <hr />
+      <button onClick={props.looksGood}>Looks Good!</button>
+      <button>Edit my RSVP</button>
+      <button onClick={() => props.delete(thisUser.attendantId)}>Delete my RSVP</button>
+    </div>
+  )
+}
+
+export default RSVPConfirm;
