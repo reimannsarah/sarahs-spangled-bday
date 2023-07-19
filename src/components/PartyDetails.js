30768dd29445e5b92a4150112833ab1f5febe1ae
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-19 16:22:35 -0700

    update party details
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/PartyDetails.js
+++ src/components/PartyDetails.js
@@ -1,13 +1,19 @@
+import berli from '../imgs/berli-smash.gif'
+
 function PartyDetails() {
   return (
-    <div>
-      <h1>Sarah's Spangled Birthday Smash</h1>
+    <div className="party-details">
+      <h1>LET'S RAGE </h1>
       <h4>When: </h4>
-      <p>September 16 at 6pm</p>
+      <p>September 9th at 6pm</p>
       <h4>Where: </h4>
       <p>Sarah and Lucy's Castle</p>
+      <p>2126 NE 49th Avenue</p>
       <h4>What: </h4>
       <p>Rage Room get your crank on and bring something to smash</p>
+      <h4>Hosted by:</h4>
+      <p>Lucy and Sarah and Emma</p>
+      <img alt="berli smashing a vase with a baseball bat" src={berli} />
     </div>
   )
 }
