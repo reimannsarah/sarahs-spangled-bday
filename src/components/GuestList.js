8c3c307d8dde761dc78614baaeaa6e03d499c75d
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-19 16:21:38 -0700

    add gifs to guest list and reorder
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import bananapickle from '../imgs/banana-pickle.gif';
 
 function GuestList(props) {
   const guestList = props.guestList;
   return (
-    <div>
+    <div className="guest-list">
       <h1>THE GUESTS</h1>
+      <img className="banana-pickle" alt="gif of banana and pickle dancing" src={bananapickle}/>
+      <img className="banana-pickle" alt="gif of banana and pickle dancing" src={bananapickle}/>
       <ul>
         {guestList.map((guest, index) =>
           <li key={index}>
             <h3>{guest.name}</h3>
-            <p>{guest.email}</p>
-            <p>Species: {guest.species}</p>
-            <p>Do You Believe? {guest.doYouBelieve}</p>
+            <p><strong>Species:</strong> {guest.species}</p>
+            <p><strong>Do You Believe?</strong> {guest.doYouBelieve}</p>
+            <p id="email">{guest.email}</p>
           </li>
         )}
       </ul>
