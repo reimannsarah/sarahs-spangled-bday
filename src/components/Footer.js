5df8e88b832da70019dc1cc933cbcb0aa954e50d
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-21 15:03:49 -0700

    move gif to header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import scooter from "../imgs/scooter.gif";
 function Footer() {
   return (
     <div className="footer">
-      <div className="scooter">
+      {/* <div className="scooter">
         <img alt="abstract moving art" src={scooter} />
-      </div>
+      </div> */}
       <p>&#xa9; 2023 Emma Gerigscott and Sarah Reimann, Pancreas Designs, Ladies IT Department</p>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import scooter from "../imgs/scooter.gif";
 
 function Header() {
   return(
     <div className="header">
       <h1>SARAH'S 27th BIRTHDAY SMASH</h1>
+      <div className="scooter">
+        <img alt="abstract moving art" src={scooter} />
+      </div>
     </div>
   )
 }
