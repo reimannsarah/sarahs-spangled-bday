23da093ce9d4a9b519054fa222c82bb302d3ef28
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-23 16:03:03 -0700

    update party details

--- src/components/PartyDetails.js
+++ src/components/PartyDetails.js
@@ -6,12 +6,12 @@ function PartyDetails() {
     <div className="party-details">
       <h1>LET'S RAGE </h1>
       <h4>When: </h4>
-      <p>September 9th at 6pm</p>
+      <p>September 9th at 7pm</p>
       <h4>Where: </h4>
       <p>Sarah and Lucy's Castle</p>
       <p>2126 NE 49th Avenue</p>
       <h4>What: </h4>
-      <p>Rage Room get your crank on and bring something to smash</p>
+      <p>We're setting up a rage room in my backyard. Bring something to smash!! Either you can smash it or I will!! Whatever you want.</p>
       <h4>Hosted by:</h4>
       <p>Lucy and Sarah and Emma</p>
       <img alt="berli smashing a vase with a baseball bat" src={berli} />
